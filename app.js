8db08fb7579bdaf474e681c472cbee3881903d96
Bilgekaan36 <b.yilmaz@workdigital.de>
2023-05-27 23:16:54 +0300

    [main] add routes and went to mvc pattern also add some default styling

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const rootDir = require("./util/path");
 
-app.use(bodyParser.urlencoded({extended: false}));
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(adminRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
-})
+app.use("/", (req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+});
 
-
-app.listen(3000); 
+app.listen(3000);
